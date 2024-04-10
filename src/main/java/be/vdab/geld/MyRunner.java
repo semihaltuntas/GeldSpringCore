d7795d1f7b5c74fcd2a6ae4a84402afa9a163a24
semihkenan <altuntas.semih34@gmail.com>
2024-04-10 17:25:06 +0200

    5.dag SpringCore

--- src/main/java/be/vdab/geld/MyRunner.java
+++ src/main/java/be/vdab/geld/MyRunner.java
@@ -1,8 +1,13 @@
 package be.vdab.geld;
 
+import be.vdab.geld.mensen.Mens;
 import be.vdab.geld.mensen.MensService;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
 
+import java.util.Scanner;
+
+@Component
 public class MyRunner implements CommandLineRunner {
     private final MensService mensService;
 
@@ -12,7 +17,16 @@ public class MyRunner implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        mensService.findAll().forEach(
-                mens -> System.out.println(mens.getNaam()));
+        var scanner = new Scanner(System.in);
+        System.out.println("Naam: ");
+        var naam = scanner.nextLine();
+        System.out.println("Geld: ");
+        var geld = scanner.nextBigDecimal();
+        var mens = new Mens(0,naam,geld);
+        var nieuwId = mensService.create(mens);
+        System.out.println("Id van deze mens: "+ nieuwId);
+
+//        mensService.findAll().forEach(
+//                mens -> System.out.println(mens.getNaam() + ":"+ mens.getGeld()));
     }
 }
--- src/main/java/be/vdab/geld/mensen/MensService.java
+++ src/main/java/be/vdab/geld/mensen/MensService.java
@@ -1,19 +1,27 @@
 package be.vdab.geld.mensen;
+
 import be.vdab.geld.mensen.MensRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Isolation;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+
 @Service
-@Transactional(isolation = Isolation.READ_COMMITTED)
+@Transactional(readOnly = true)
 public class MensService {
     private final MensRepository mensRepository;
 
     public MensService(MensRepository mensRepository) {
         this.mensRepository = mensRepository;
     }
+
     public List<Mens> findAll() {
         return mensRepository.findAll();
+    }
+
+    @Transactional
+    public long create(Mens mens) {
+        return mensRepository.create(mens);
     }
 }
